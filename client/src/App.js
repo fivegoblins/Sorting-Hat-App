02db7d2c9d9e6ac3b951afda4efb8a5a28652fc3
Alexandra <alexswartz9753@gmail.com>
2018-12-13 14:54:59 -0500

    built state for App

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,24 @@ import Question from './components/Question';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0,
+      questionId: 1,
+      question: '',
+      answerOptions: [],
+      answer: '',
+      answersCount: {
+        slytherin: 0,
+        gryffindor: 0,
+        hufflepuff: 0,
+        ravenclaw: 0
+      },
+      result: ''
+    };
+  }
+  
   render() {
     return (
       <div className="App">
