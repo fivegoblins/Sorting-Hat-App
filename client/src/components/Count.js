ff2304a776287845564f5accf9d6c32aa2e37606
Alexandra <alexswartz9753@gmail.com>
2018-12-13 14:34:09 -0500

    created component Count to keep track of question count

---
+++ client/src/components/Count.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Count(props) {
+    return (
+        <div className='question-count'>
+            Question <span>{props.counter}</span> of <span>{props.total}</span>
+        </div>
+    );
+}
+
+Count.propTypes = {
+    counter: PropTypes.number.isRequired,
+    total: PropTypes.number.isRequired
+}
+
+export default Count;
